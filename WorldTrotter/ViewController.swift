6a033cfe136055995be51bdc88db0e8beea84be7
Hussein <hussein.elsherief@gmail.com>
2022-04-09 00:32:32 +0200

    - Solved silver and gold challenges

--- WorldTrotter/ViewController.swift
+++ WorldTrotter/ViewController.swift
@@ -13,6 +13,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillLayoutSubviews() {
+        setGradientBackground()
+    }
+    
+    func setGradientBackground() {
+        let colorTop = UIColor(red: 255.0/255.0, green: 100.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 77.0/255.0, blue: 50.0/255.0, alpha: 1.0).cgColor
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop,colorBottom]
+        gradientLayer.locations = [0.0,1.0]
+        gradientLayer.frame = self.view.bounds
+        
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+
+        
+    }
 
 
 }
