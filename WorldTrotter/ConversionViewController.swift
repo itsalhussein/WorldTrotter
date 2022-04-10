9b5f78f584e2baa0db6b1ed00949c1536772d202
Hussein <hussein.elsherief@gmail.com>
2022-04-10 16:36:10 +0200

    - Implemented the temperature conversion

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -8,10 +8,41 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
+    //MARK: - Outlets
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    //MARK: - Properties
+    
+    var fahrenheitValue: Measurement<UnitTemperature>?{
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
+    
+    
+    //MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateCelsiusLabel()
     }
     
     override func viewWillLayoutSubviews() {
@@ -20,9 +51,13 @@ class ConversionViewController: UIViewController {
     
     
     override func viewWillAppear(_ animated: Bool) {
-        self.view.backgroundColor = randomBackgroundColor()
+//        self.view.backgroundColor = randomBackgroundColor()
     }
     
+    
+    
+    //MARK: - Methods
+
     func setGradientBackground() {
         let colorTop = UIColor(red: 250.0/255.0, green: 60/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
         let colorBottom = UIColor(red: 250.0/255.0, green: 80/255.0, blue: 50.0/255.0, alpha: 1.0).cgColor
@@ -50,6 +85,30 @@ class ConversionViewController: UIViewController {
         return UIColor(red: r, green: g, blue: b, alpha: 1.0)
     }
     
+    func updateCelsiusLabel(){
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    
+    //MARK: - Actions
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField){
+        
+        if let text = textField.text, let value = Double(text){
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+      
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer){
+        textField.resignFirstResponder()
+    }
     
 }
 
